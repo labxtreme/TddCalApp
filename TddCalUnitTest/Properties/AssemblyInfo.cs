73738d0f255df4a5b7779b07a599aa525431a09e
lakshaya bathla <lakshaya.bathla@gmail.com>
2019-09-13 10:28:38 +0530

    Failed Test Project

---
+++ TddCalUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TddCalUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TddCalUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9fc50b7d-fd74-48fa-8fd5-820843008376")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TddCalUnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tddcalapp.library;
+
+namespace TddCalUnitTest
+{
+    [TestClass]
+    public class TestCalculator
+    {
+        [TestMethod]
+        public void Should_Return_addition_of_two_numbers()
+        {
+            Calculator testcalculator = new Calculator();
+            int num1 = 3;
+            int num2 = 2;
+            int result;
+            result = testcalculator.Add(num1, num2);
+            Assert.AreEqual(4, result, "Testing two integers 3 and 2");
+        }
+    }
+}
