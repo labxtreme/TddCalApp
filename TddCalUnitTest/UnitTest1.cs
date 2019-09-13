0bfe845e10f456333902f0da04cb35810578082b
lakshaya bathla <lakshaya.bathla@gmail.com>
2019-09-13 10:38:50 +0530

    success of the second test case in the project

--- TddCalUnitTest/UnitTest1.cs
+++ TddCalUnitTest/UnitTest1.cs
@@ -17,5 +17,15 @@ namespace TddCalUnitTest
             result = testcalculator.Add(num1, num2);
             Assert.AreEqual(5, result, "Testing two integers 3 and 2");
         }
+        [TestMethod]
+        public void Should_Return_Zero_on_passing_both_positive_and_negative_numbers()
+        {
+            Calculator testcalculator = new Calculator();
+            int num1 = 3;
+            int num2 = -3;
+            int result;
+            result = testcalculator.Add(num1, num2);
+            Assert.AreEqual(0, result, "Testing two integers 3 and -3");
+        }
     }
 }
